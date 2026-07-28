10f883b6f2e8e6b56c995e29e7acd0ffeb899e20
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-07-28 21:42:14 +0800

    Update app.js

--- parentdashboard/app.js
+++ parentdashboard/app.js
@@ -60,7 +60,7 @@ async function loadDashboard() {
     try {
         console.log("Fetching dashboard data for user ID:", userId);
 
-        const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:wtEDiEuV/parents?user_id=' + userId, {
+     const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:wtEDiEuV/parents?user_id=' + userId + '&admin_type_id=' + adminTypeId, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
